5feec287177696c35c8a64a23c5183d830c59215
WebabyApps <webabyrocks@icloud.com>
2026-01-14 13:57:44 +0100

    Initial Commit

---
+++ NewColours Watch App/NewColoursApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewColoursApp.swift
+//  NewColours Watch App
+//
+//  Created by Tomasz Szornel on 1/14/26.
+//
+
+import SwiftUI
+
+@main
+struct NewColours_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
